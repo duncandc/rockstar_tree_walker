6720f9781bfa462cbe0b5115c0ddcfbd8f56fcea
Andrew Hearin <andrew.hearin@yale.edu>
2016-08-26 17:23:32 -0400

    Fixed bug in trunk_generator

--- ascii_subvolume_walker.py
+++ ascii_subvolume_walker.py
@@ -1,13 +1,13 @@
 """
 """
-
+from time import time
 
 def mmp_row_generator(tree_fname, mmp_col_index, desc_id_col_index, halo_id_col_index,
         *colnums_to_yield):
     """ Given an input hlist ASCII file, yield the desired columns of all rows
     storing the main progenitors in the file.
     """
-
+    start = time()
     with open(tree_fname, 'r') as f:
 
         # Skip the header, extracting num_trees
@@ -21,17 +21,21 @@ def mmp_row_generator(tree_fname, mmp_col_index, desc_id_col_index, halo_id_col_
             try:
                 raw_line = next(f)
                 if raw_line[0] == '#':
-                    current_tree_root_id = raw_line.strip().split()[1]
+                    current_trunk_id = raw_line.strip().split()[1]
                 else:
                     list_of_strings = raw_line.strip().split()
                     mmp = list_of_strings[mmp_col_index]
                     desc_id = list_of_strings[desc_id_col_index]
                     halo_id = list_of_strings[halo_id_col_index]
                     string_data = tuple(list_of_strings[idx] for idx in colnums_to_yield)
-                    yield_current_line = ((mmp == '1') & (desc_id == current_tree_root_id) |
-                        (current_tree_root_id == halo_id))
+                    yield_current_line = ((mmp == '1') & (desc_id == current_trunk_id) |
+                        (current_trunk_id == halo_id))
                     if yield_current_line:
+                        current_trunk_id = halo_id
                         yield string_data
 
             except StopIteration:
                 break
+
+    end = time()
+    # print("Total runtime = {0:.2f} seconds".format(end-start))
